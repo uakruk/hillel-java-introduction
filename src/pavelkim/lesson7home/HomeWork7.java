f3a312338e0a448b766df8439e38f40e0b1f9061
kim-alterpost <kim@alterpost.org>
2020-11-06 15:38:06 +0200

    Home work lesson 7 Pavel Kim

--- src/pavelkim/lesson7home/HomeWork7.java
+++ src/pavelkim/lesson7home/HomeWork7.java
@@ -1,2 +1,31 @@
-package pavelkim.lesson7home;public class HomeWork7 {
+package pavelkim.lesson7home;
+
+public class HomeWork7 {
+    public double averageWithin(int[] array) {
+        int sum = 0;
+        for (int j : array) {
+            sum += j;
+            System.out.println(sum);
+        }
+        return (double) sum / array.length;
+    }
+
+    public int maxArrayValue(int [] array) {
+        int max = array[0];
+        for (int j : array) {
+            if(max < j) {
+                max = j;
+            }
+        }
+        return max;
+    }
+    public int minArrayValue(int [] array) {
+        int min = array[0];
+        for (int j : array) {
+            if(min > j) {
+                min = j;
+            }
+        }
+        return min;
+    }
 }
--- src/pavelkim/lesson7home/Main.java
+++ src/pavelkim/lesson7home/Main.java
@@ -1,2 +1,19 @@
-package pavelkim.lesson7home;public class Main {
+package pavelkim.lesson7home;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        HomeWork7 homeWork7 = new HomeWork7();
+
+        int [] array = new int[] {33, 23, 22, 44, 11};
+        System.out.println(homeWork7.averageWithin(array));
+
+        System.out.println(homeWork7.maxArrayValue(array));
+
+        System.out.println(homeWork7.minArrayValue(array));
+
+    }
+
 }
