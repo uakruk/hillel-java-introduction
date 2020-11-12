3170049a1c6f7305982087016c80e0165bb4124b
alexeyananchenko <LQp\(BRVtL5_!yV>
2020-11-12 22:10:02 +0200

    doing homework

---
+++ src/com/hillel/java/introduction/oleksiiananchenko/HW9/Devteamone.java
@@ -0,0 +1,6 @@
+package com.hillel.java.introduction.oleksiiananchenko.HW9;
+
+public class Devteamone extends sdlc{
+    String firstPhase = Training();
+
+}
---
+++ src/com/hillel/java/introduction/oleksiiananchenko/HW9/sdlc.java
@@ -0,0 +1,35 @@
+package com.hillel.java.introduction.oleksiiananchenko.HW9;
+
+public class sdlc {
+    private boolean passed;
+    private double devs;
+
+    public void Training(){
+        System.out.println("Trainig phase: ");
+    }
+
+    public void Requirements(){
+        System.out.println("Requirements phase: ");
+    }
+
+    public void Design(){
+        System.out.println("Design phase: ");
+    }
+
+    public void Implementation(){
+        System.out.println("Implementation phase: ");
+    }
+
+    public void Verification(){
+        System.out.println("Verification phase: ");
+    }
+
+    public void Release(){
+        System.out.println("Release phase: ");
+    }
+
+    public void Response(){
+        System.out.println("Response phase: ");
+    }
+
+}
