837b0a9957d1f6e4a1112191147b1ad200b6d764
kim-alterpost <kim@alterpost.org>
2020-11-17 19:36:28 +0200

    Домашнее задание 11 - Редактирование ДЗ 8

--- src/pavelkim/lesson8/Zoo.java
+++ src/pavelkim/lesson8/Zoo.java
@@ -1,13 +1,14 @@
 package pavelkim.lesson8;
 
-public class Zoo {
+public class    Zoo {
 
     public static void main(String [] args) {
-//        Cat vasya = new Cat();
-//
-//        vasya.eat();
-//        vasya.kak();
-//        vasya.size = 10;
-//        vasya.weight = 10;
+        Cat vasya = new Cat(10, 12, "red");
+        vasya.sound();
+        System.out.println(vasya.color);
+
+        Dog dog = new Dog(20, 25, "Jack");
+        dog.sound();
+        System.out.println(dog.color());
     }
 }
